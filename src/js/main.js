d496b197324e444d2a757a949228ea07da4edc29
Justus Deitert <mail@justusdeitert.de>
2018-07-11 21:47:12 +0200

    Add .editorconfig with 4-space indentation

--- src/js/main.js
+++ src/js/main.js
@@ -4,30 +4,30 @@ import '../scss/styles.scss';
 
 // DOM Ready
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('%c DOM Ready!!', 'font-weight: bold;');
-  console.log('%c Hello Visitor! :)', 'color: green; font-weight: bold; font-size: 2em');
+    console.log('%c DOM Ready!!', 'font-weight: bold;');
+    console.log('%c Hello Visitor! :)', 'color: green; font-weight: bold; font-size: 2em');
 });
 
 // Window Load - all assets loaded
 window.addEventListener('load', function() {
-  console.log('All assets are loaded');
-  
-  const leftImage = document.querySelector('.left-image');
-  const footer = document.querySelector('footer');
-  const body = document.querySelector('body');
-  
-  if (leftImage && footer) {
-    leftImage.style.top = footer.offsetHeight + 'px';
-    body.classList.remove('loading');
-  }
+    console.log('All assets are loaded');
+    
+    const leftImage = document.querySelector('.left-image');
+    const footer = document.querySelector('footer');
+    const body = document.querySelector('body');
+    
+    if (leftImage && footer) {
+        leftImage.style.top = footer.offsetHeight + 'px';
+        body.classList.remove('loading');
+    }
 });
 
 // Window Resize
 window.addEventListener('resize', function() {
-  const leftImage = document.querySelector('.left-image');
-  const footer = document.querySelector('footer');
-  
-  if (leftImage && footer) {
-    leftImage.style.top = footer.offsetHeight + 'px';
-  }
+    const leftImage = document.querySelector('.left-image');
+    const footer = document.querySelector('footer');
+    
+    if (leftImage && footer) {
+        leftImage.style.top = footer.offsetHeight + 'px';
+    }
 });
